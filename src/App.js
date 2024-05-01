87f43145dcef3cd228374861c17b4943ff6d80e8
Magomed <ingushotdushi@mail.ru>
2024-05-01 18:34:20 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,125 +1,148 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
-import MovieCard from "./components/MovieCard";
+import MovieCard from './components/MovieCard'
 import './App.scss'
-import YouTube from 'react-youtube';
+import YouTube from 'react-youtube'
 
 function App() {
-    const IMAGE_PATH = 'https://image.tmdb.org/t/p/original'
-    const API_URL = 'https://api.themoviedb.org/3'
+	const IMAGE_PATH = 'https://image.tmdb.org/t/p/original'
+	const API_URL = 'https://api.themoviedb.org/3'
 
-    const [movies, setMovies] = useState([])
-    const [searchKey, setSearchKey] = useState('')
-    const [selectedMovie, setSelectedMovie] = useState({});
-    const [playerTrailer, setPlayerTrailer] = useState(false);
+	const [movies, setMovies] = useState([])
+	const [searchKey, setSearchKey] = useState('')
+	const [selectedMovie, setSelectedMovie] = useState({})
+	const [playerTrailer, setPlayerTrailer] = useState(false)
 
+	const fetchMovies = async (searchKey) => {
+		const type = searchKey ? 'search' : 'discover'
+		const {
+			data: { results },
+		} = await axios
+			.get(`${API_URL}/${type}/movie`, {
+				params: {
+					api_key: process.env.REACT_APP_MOVIE_API_KEY,
+					query: searchKey,
+				},
+			})
+			.catch((err) => console.log(err))
 
-    const fetchMovies = async (searchKey) => {
-        const type = searchKey ? 'search' : 'discover'
-        const { data: { results } } = await axios.get(`${API_URL}/${type}/movie`, {
-            params: {
-                api_key: process.env.REACT_APP_MOVIE_API_KEY,
-                query: searchKey
-            },
-        }).catch(err => console.log(err))
+		setMovies(results)
+		await selectMovie(results[0])
+	}
 
-        setMovies(results)
-        await selectMovie(results[0])
-    }
+	const fetchMovie = async (id) => {
+		const { data } = await axios.get(`${API_URL}/movie/${id}`, {
+			params: {
+				api_key: process.env.REACT_APP_MOVIE_API_KEY,
+				append_to_response: 'videos',
+			},
+		})
 
-    const fetchMovie = async (id) => {
-        const { data } = await axios.get(`${API_URL}/movie/${id}`, {
-            params: {
-                api_key: process.env.REACT_APP_MOVIE_API_KEY,
-                append_to_response: 'videos'
-            }
-        })
+		return data
+	}
 
-        return data
-    }
+	useEffect(() => {
+		fetchMovies()
+	}, [])
 
-    useEffect(() => {
-        fetchMovies()
-    }, [])
+	const selectMovie = async (movie) => {
+		setPlayerTrailer(false)
+		const data = await fetchMovie(movie.id)
+		setSelectedMovie(data)
+		window.scrollTo(0, 0)
+	}
 
-    const selectMovie = async (movie) => {
-        setPlayerTrailer(false)
-        const data = await fetchMovie(movie.id)
-        setSelectedMovie(data)
-        window.scrollTo(0, 0)
-    }
+	const renderMovies = () =>
+		movies.map((movie) => (
+			<MovieCard key={movie.id} movie={movie} selectMovie={selectMovie} />
+		))
 
-    const renderMovies = () => (
-        movies.map((movie) => (
-            <MovieCard
-                key={movie.id}
-                movie={movie}
-                selectMovie={selectMovie} />
-        ))
-    )
+	const searchMovies = (event) => {
+		event.preventDefault()
+		fetchMovies(searchKey)
+	}
 
-    const searchMovies = (event) => {
-        event.preventDefault()
-        fetchMovies(searchKey)
-    }
+	const renderTrailer = () => {
+		console.log(selectedMovie.videos.results)
+		const trailer = selectedMovie.videos.results.find((video) => {
+			console.log(video)
+			return video.name === 'Official Trailer'
+		})
+		const key = trailer ? trailer.key : selectedMovie.videos.results[0].key
+		return (
+			<YouTube
+				videoId={key}
+				className={'youtube-container'}
+				opts={{
+					width: '100%',
+					height: '100%',
+					playerVars: {
+						autoplay: 1,
+						disablekb: 1,
+						controls: 1,
+						color: 'red',
+					},
+				}}
+			/>
+		)
+	}
 
-    const renderTrailer = () => {
-        const trailer = selectedMovie.videos.results.find((vid) => vid.name === 'Official Trailer')
-        const key = trailer ? trailer.key : selectedMovie.videos.results[0].key
-        return (
-            <YouTube
-                videoId={key}
-                className={'youtube-container'}
-                opts={{
-                    width: '100%',
-                    height: '100%',
-                    playerVars: {
-                        autoplay: 1,
-                        disablekb: 1,
-                        controls: 1,
-                        color: 'red'
-                    },
-                }}
-            />
-        )
-    }
+	return (
+		<div className="App">
+			<header>
+				<div className="header-content centering">
+					<a className="header-link" href="/">
+						KINOPOISK
+					</a>
 
-    return (
-        <div className='App'>
-            <header>
-                <div className="header-content centering">
+					<form onSubmit={searchMovies}>
+						<input
+							className="header-input"
+							placeholder="Поиск..."
+							type="text"
+							onChange={(e) => setSearchKey(e.target.value)}
+						/>
+						<button className="header-btn" type="submit">
+							Search
+						</button>
+					</form>
+				</div>
+			</header>
 
-                    <a className='header-link' href="/">KINOPOISK</a>
+			<div
+				className="hero"
+				style={{
+					backgroundImage: `url('${IMAGE_PATH}${selectedMovie.backdrop_path}')`,
+				}}
+			>
+				<div className="hero-content centering ">
+					{playerTrailer && (
+						<button
+							className="hero-btn hero-btn-close"
+							onClick={() => setPlayerTrailer(false)}
+						>
+							Close
+						</button>
+					)}
 
-                    <form onSubmit={searchMovies}>
-                        <input className='header-input' placeholder='Поиск...' type="text" onChange={(e) => setSearchKey(e.target.value)} />
-                        <button className='header-btn' type='submit' >Search</button>
-                    </form>
-                </div>
-            </header>
+					{selectedMovie.videos && playerTrailer ? renderTrailer() : null}
 
-            <div className="hero" style={{ backgroundImage: `url('${IMAGE_PATH}${selectedMovie.backdrop_path}')` }}>
-                <div className="hero-content centering ">
-                    {playerTrailer && (
-                        <button className='hero-btn hero-btn-close' onClick={() => setPlayerTrailer(false)}>Close</button>
-                    )}
+					{!playerTrailer && (
+						<h1 className="hero-title">{selectedMovie.title}</h1>
+					)}
+					<button className="hero-btn" onClick={() => setPlayerTrailer(true)}>
+						Play trailer
+					</button>
 
+					{selectedMovie.overview ? (
+						<p className="hero-overview">{selectedMovie.overview}</p>
+					) : null}
+				</div>
+			</div>
 
-
-                    {selectedMovie.videos && playerTrailer ? renderTrailer() : null}
-
-                    <h1 className='hero-title'>{selectedMovie.title}</h1>
-                    <button className='hero-btn' onClick={() => setPlayerTrailer(true)}>Play trailer</button>
-
-                    {selectedMovie.overview ? <p className='hero-overview'>{selectedMovie.overview}</p> : null}
-                </div>
-            </div>
-
-            <div className=" container centering">
-                {renderMovies()}
-            </div>
-        </div>
-    )
+			<div className=" container centering">{renderMovies()}</div>
+		</div>
+	)
 }
 
-export default App;
+export default App
